256b5961e9262ad86720f3961a289de50605288e
asaba <asaba@oa-cagliari.inaf.it>
2016-11-28 22:36:42 +0100

    Changed host info for email

--- sendlogmail.py
+++ sendlogmail.py
@@ -11,12 +11,12 @@ import smtplib
 # from email.mime.text import MIMEText
 # from email.utils import COMMASPACE, formatdate
 
-sender = 'storage@storage-200.srt.inaf.it'
+sender = 'storage@srtmain.oa-cagliari.inaf.it'
 receivers = ['asaba@oa-cagliari.inaf.it', "sabahome@gmail.com", "fara@oa-cagliari.inaf.it"]
 
 header_message = """From: Storage Manager <storage@storage-200.srt.inaf.it>
 To: Andrea Saba <asaba@oa-cagliari.inaf.it>
-Subject: [storage-200.srt.inaf.it] rsync log
+Subject: [srtmain.oa-cagliari.inaf.it] rsync log
 
 """
 
